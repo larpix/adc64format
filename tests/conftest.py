5587064fff7874a5bd318686b5b33c44826e2234
peter-madigan <pmadigan@berkeley.edu>
2022-11-02 21:43:25 -0700

    Add conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+import os
+
+test_dir = os.path.dirname(__file__)
+_example_files = [
+    os.path.join(test_dir, 'test_0a7a314c.data'),
+    os.path.join(test_dir, 'test_0a7b54bd.data')
+]
+
+@pytest.fixture
+def example_files():
+    return _example_files
+
+@pytest.fixture(params=_example_files)
+def example_file(request):
+    return request.param
+    

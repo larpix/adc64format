b107f91bdfd797d5b485d47572ac83f60d9025f3
peter-madigan <pmadigan@berkeley.edu>
2022-11-03 22:10:01 -0700

    Lookup block sizes when opening a file and adapt datasets accordingly

--- adc64_to_hdf5.py
+++ adc64_to_hdf5.py
@@ -12,7 +12,7 @@
 #   pip install numpy h5py tqdm
 #
 # usage:
-#   python adc64_to_hdf5.py <number of samples in waveforms> <input file, .data> <output file, .h5>
+#   python adc64_to_hdf5.py <input file, .data> <output file, .h5>
 #
 # output hdf5 file has the following datasets:
 #   "header": adc64 "Time Header"
@@ -31,36 +31,58 @@ import sys
 import tqdm
 from collections import defaultdict
 
-from adc64format import ADC64Reader, dtypes
+import adc64format
 
-nsamples = int(sys.argv[-3])
 input_file = sys.argv[-2]
 output_file = sys.argv[-1]
 
 WRITE_BUFFER = 1024-1
 
-dtypes = dtypes.copy()
-dtypes['data'] = dtypes['data'](nsamples)
-dtypes['ref'] = np.dtype([('start', 'i4'), ('stop', 'i4')])
+print('''
+*************
+ADC64 -> HDF5
+*************
+''', end='')
 
-print(f'Opening input file {input_file}...', end=' ')
-with ADC64Reader(input_file) as reader:
-    print(f'Creating new file at {output_file}...', end=' ')
+print(f'opening {input_file}')
+with adc64format.ADC64Reader(input_file) as reader:
+    print('success!')
+
+    print('reading first block')
+    _, nbytes, test_event = adc64format.parse_chunk(reader.streams[0])
+    nchannels = len(test_event['data'])
+    nsamples = test_event['data'].dtype['voltage'].shape[-1]
+    print(f'block size is {nbytes}B, with {nchannels} channels and {nsamples} samples')
+
+    print('checking file length')
+    size = reader.streams[0].seek(0, 2)
+    reader.streams[0].seek(0, 0)
+    chunk_size = adc64format.chunk_size(reader.streams[0])
+    print(f'file contains {size // chunk_size} events')
+    
+    print(f'creating {output_file}')
     with h5py.File(output_file, 'w') as fo:
+        print('success!')
 
+        print('generating datasets')
+        dtypes = adc64format.dtypes.copy()
+        dtypes['data'] = adc64format.dtypes['data'](nsamples)
+        dtypes['ref'] = np.dtype([('start', 'i4'), ('stop', 'i4')])
         for key in dtypes:
-            fo.create_dataset(key, (0,), maxshape=(None,), dtype=dtypes[key], compression='gzip', shuffle=True)
+            initial_shape = (size // chunk_size,) if key != 'data' else (nchannels * size // chunk_size,)
+            fo.create_dataset(key, initial_shape, maxshape=(None,), dtype=dtypes[key], compression='gzip', shuffle=True)
         dset_keys = list(fo.keys())
 
         ptr = defaultdict(int)
         data = defaultdict(list)
-        print('datasets: ' + ', '.join(dset_keys) + '...', end=' ')
+        print('initial dataset sizes:\n' + '\n'.join([f'  {k}: {fo[k].shape}' for k in dset_keys]))
 
-        print('Done!')
+        print('\n*************\n   CONVERT\n*************')
 
-        with tqdm.tqdm(unit=' events', smoothing=0) as pbar:
+        with tqdm.tqdm(total=size//chunk_size, unit=' events', smoothing=0) as pbar:
             while True:
-                event = reader.next()
+                events = reader.next()
+                event = events[0] if events is not None else None
 
                 if event is not None and event['header'][0] is not None:
                     for key in event:
@@ -95,8 +117,9 @@ with ADC64Reader(input_file) as reader:
 
                 pbar.update()
 
+        print('success!')
         for key in dset_keys:
             fo[key].resize((ptr[key],))
-            print(f'"{key}" final shape of {ptr[key]}')
+        print('final dataset sizes:\n' + '\n'.join([f'  {k}: {fo[k].shape}' for k in dset_keys]))
 
-print('Conversion complete!')
+print('\n*************')
